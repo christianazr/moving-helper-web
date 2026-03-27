69632ebf74ae72cbd145f835b502b5e6d343d4c6
christianazr <christian.azana.reina@gmail.com>
2026-03-27 16:56:56 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://iluwuzmoussdvycmfqol.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsdXd1em1vdXNzZHZ5Y21mcW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MDM4NzQsImV4cCI6MjA5MDE3OTg3NH0.9VBs4ppthf_2AJd8CPAWN6659GL0HdAdYRefthBkLLs";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
